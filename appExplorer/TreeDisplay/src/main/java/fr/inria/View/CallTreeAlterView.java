8eb17e55a2f8e82043b96574b4efab9561cd200c
Nicolas Harrand <nicolas.harrand@gmail.com>
2017-05-02 20:38:39 +0200

    new imgs & add constructors tracing in yalta

--- appExplorer/TreeDisplay/src/main/java/fr/inria/View/CallTreeAlterView.java
+++ appExplorer/TreeDisplay/src/main/java/fr/inria/View/CallTreeAlterView.java
@@ -35,7 +35,8 @@ public class CallTreeAlterView  extends PApplet {
 
         TreeCallUtils.label(t, e.packages, e.defaultLevel);
         //t= TreeCallUtils.from(t, "java.lang.reflect.Method.invoke");
-        t= TreeCallUtils.from(t, "QuickSortTest.quickSort()");
+        //t = TreeCallUtils.from(t, "QuickSortTest.quickSort()");
+        t = TreeCallUtils.from(t, "QuickSort.sort(");
 
 
 
